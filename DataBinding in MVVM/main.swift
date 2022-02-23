d4e1750c07b1fc0df3f5b8064bd076a798195341
Sait CİHANER <saitcihaner@hotmail.com>
2022-02-23 22:02:08 +0300

    DelegatePattern

--- DataBinding in MVVM/main.swift
+++ DataBinding in MVVM/main.swift
@@ -9,3 +9,34 @@ import Foundation
 
 print("Hello, World!")
 
+
+// With Delegate
+
+protocol ObserveDelegate:class{
+    func propertyChanged(newValue:Int)
+}
+
+class ObjectToObserve{
+    private var foo:Int = 0
+    weak var delegate : ObserveDelegate?
+    func changeValue(){
+        foo += 1
+        delegate?.propertyChanged(newValue: foo)
+    }
+}
+
+
+class MyObserver : ObserveDelegate{
+    var objectToObserve = ObjectToObserve.init()
+    func propertyChanged(newValue: Int) {
+        print("Value has been changed to \(newValue)")
+    }
+    
+    init(){
+        objectToObserve.delegate = self
+    }
+    
+}
+
+let observer = MyObserver.init()
+observer.objectToObserve.changeValue()
